22ad7422ef49a48bd8fdf7ec76b8512182f4c704
Lydia Cupery <lac26@hillis.cs.calvin.edu>
2016-11-16 12:52:38 -0500

    lab11

---
+++ app/scripts/index.js
@@ -0,0 +1,15 @@
+/* imports the comment box class to create content
+Lydia Cupery */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import CommentBox from './commentBox';
+
+import '../css/base.css';
+
+ReactDOM.render(
+    <CommentBox url="/api/comments" pollInterval={2000}/>,
+    document.getElementById('content')
+);
