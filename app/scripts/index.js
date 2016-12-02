2236cd97a3755c3032e1257d3bac636e5fac9f08
Lydia Cupery <lac26@babbage.cs.calvin.edu>
2016-12-01 19:16:19 -0500

    updates

---
+++ app/scripts/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, Redirect, browserHistory } from 'react-router';
+
+import CommentBox from './commentBox';
+import CommentEdit from './commentEdit';
+
+import '../css/base.css';
+
+import { StoreTools } from './flux';
+StoreTools.startLoadingComments();
+
+
+ReactDOM.render((
+    <Router history={browserHistory}>
+        <Route path="/" component={CommentBox}/>
+        <Route path="/:id" component={CommentEdit} />
+    </Router>
+), document.getElementById('content')
+);
